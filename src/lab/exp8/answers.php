98b8880c592fb6cee287a8e542709c488e0ba501
aakarsh7599 <32263537+aakarsh7599@users.noreply.github.com>
2018-06-14 23:53:24 +0530

    Add files via upload
    Additional files for quiz evaluation.

---
+++ src/lab/exp8/answers.php
@@ -0,0 +1,23 @@
+<?php
+echo "<html>
+<head></head>";
+echo "<body class=\"page_bg\">";
+echo '	<h1>Correct Answers</h1>
+<br>
+<b>Q1.</b>
+                <b>ECB stands for </b><br>
+                A.Electronic code Book<br>
+                <br>
+<b>Q2.</b>      <b> Which among the following is not CPA secure? </b> <br>A. ECB <br>
+                  <br>
+<b>Q3.</b>                
+                <b>Which of the following are block ciphers? </b><br>
+               A.All<br>
+                <br>
+</ol>';
+echo "
+</body>
+</html>";
+?>
+<body bgcolor="#E6E6FA">
+</body>
