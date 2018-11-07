48d3526e42278ddd8136e726b1d509fd1ee65730
DevikaBoddu <40534152+DevikaBoddu@users.noreply.github.com>
2018-11-07 19:29:29 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@
 $(function() {
     $('body').on('click', '.page-scroll a', function(event) {
         var $anchor = $(this);
-        $('html, body').stop().animate({
+        $("html, body").stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
@@ -33,5 +33,5 @@ $('body').scrollspy({
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+    $(".navbar-toggle:visible").click();
 });
