23bb89bb37e9cdc1a923f040911dca59732b6373
DevikaBoddu <40534152+DevikaBoddu@users.noreply.github.com>
2018-11-07 22:04:31 +0530

    Update exp1.js

--- src/lab/js/exp1.js
+++ src/lab/js/exp1.js
@@ -8,6 +8,7 @@ function Shift_Encrypt(src, dst) {
     if(plaintext.length < 1){ alert("please enter some plaintext"); return; }    
     var shift = parseInt(document.getElementById("shift_key").value);
     var ciphertext = "";    var re = /[a-z]/;
+    var i;
     for(i=0; i<plaintext.length; i++){ 
         if(re.test(plaintext.charAt(i))) ciphertext += String.fromCharCode((plaintext.charCodeAt(i) - 97 + shift)%26 + 97); 
         else ciphertext += plaintext.charAt(i); 
