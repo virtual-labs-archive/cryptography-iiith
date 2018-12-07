72f9d2032757885da094bf7782e0d6a1b8a4e24b
samhithavootkoor <17311a05q4@sreenidhi.edu.in>
2018-12-07 20:41:09 +0530

    fix3

--- src/lab/js/vernam.js
+++ src/lab/js/vernam.js
@@ -1,8 +1,8 @@
-var PLAIN_TEXT_LEN = 8;
-var current_encryption = "";
+var PLAINTEXTLEN = 8;
+var currentEncryption = "";
 
 function validKey() {
-    key = document.getElementById("user_key").value;
+    key = document.getElementById("userKey").value;
     var i = 0;
     for(i=0; i<key.length; i++){
 	if (key.charAt(i) !== "0" && key.charAt(i)!== "1") {
@@ -50,7 +50,7 @@ function AND(a, b) {
     return "0";
 }
 
-function Vernam_Encrypt() {
+function VernamEncrypt() {
     plaintext = document.getElementById("p").value;
     key = document.getElementById("key").value;
     if(plaintext.length < 1){ alert("please enter some plaintext (binary only)"); return; }
@@ -62,7 +62,7 @@ function Vernam_Encrypt() {
     document.getElementById("c").value = ciphertext;
 }
 
-function Vernam_Decrypt(f){
+function VernamDecrypt(f){
     ciphertext = document.getElementById("c").value;
     key = document.getElementById("key").value;
     if(ciphertext.length < 1){ alert("please enter some ciphertext (binary only)"); return; }
@@ -74,7 +74,7 @@ function Vernam_Decrypt(f){
     document.getElementById("p").value = plaintext;
 }
  
-function Vernam_RandSequence(len){ 
+function VernamRandSequence(len){ 
     var keylen = len; 
     ret=""; 
     for(i=0; i<keylen; i++) {
@@ -84,19 +84,19 @@ function Vernam_RandSequence(len){
 }
 
 function makeEqualProbability(seq) {
-    var eq_seq = "";
+    var eqSeq = "";
     for(i=0;i<seq.length;i++) {
         if((seq.charAt(i) === "0")) {
-	    eq_seq += "01";
+	    eqSeq += "01";
 	}
 	else {
-	    eq_seq += "10";
+	    eqSeq += "10";
 	}
     }
-    return eq_seq;
+    return eqSeq;
 }
 
-function Vernam_RandKey(){
+function VernamRandKey(){
     plaintext = document.getElementById("p").value;
     ciphertext = document.getElementById("c").value;
     var keylen;
@@ -106,11 +106,11 @@ function Vernam_RandKey(){
     else {
 	keylen = ciphertext.length;
     }
-    document.getElementById("key").value = Vernam_RandSequence(keylen); 
+    document.getElementById("key").value = VernamRandSequence(keylen); 
 }
 
 function equalProbabilitySequence(len) {
-    randomSeq = Vernam_RandSequence(len);
+    randomSeq = VernamRandSequence(len);
     equalProbSeq = makeEqualProbability(randomSeq);
     seq = "";
     for(i=0;i<len;i++) {
@@ -119,67 +119,67 @@ function equalProbabilitySequence(len) {
     return seq;
 }
 
-function shrink_key(key) {
-    var new_key = "";
-    for(i=0;i<PLAIN_TEXT_LEN;i++) {
-	new_key += key.charAt(i);
+function shrinkKey(key) {
+    var newKey = "";
+    for(i=0;i<PLAINTEXTLEN;i++) {
+	newKey += key.charAt(i);
     }
-    return new_key;
+    return newKey;
 }
 
-function expand_key(key) {
+function expandKey(key) {
     var index = 0;
-    while (key.length < PLAIN_TEXT_LEN) {
+    while (key.length < PLAINTEXTLEN) {
 	key += key.charAt(index);
 	index ++;
     }
     return key;
 }
 
-function resize_key(key) {
-    var key_len = key.length;
-    if (key_len > PLAIN_TEXT_LEN) {
-	key = shrink_key(key);
+function resizeKey(key) {
+    var keyLen = key.length;
+    if (keyLen > PLAINTEXTLEN) {
+	key = shrinkKey(key);
     }
-    else if (key_len < PLAIN_TEXT_LEN) {
-	key = expand_key(key);
+    else if (keyLen < PLAINTEXTLEN) {
+	key = expandKey(key);
     }
     return key;
 }
 
 function encrypt(plaintext, key) {
-    key = resize_key(key);
+    key = resizeKey(key);
     //encrypt the plaintext with the key now.
-    var cipher_text = "";
-    var encryption_technique = current_encryption;
+    var cipherText = "";
+    var encryptionTechnique = currentEncryption;
     var i = 0;
-    for(i=0;i<PLAIN_TEXT_LEN;i++) {
-	if(encryption_technique.charAt(i) === "0") {
-	    cipher_text += XOR(plaintext.charAt(i), key.charAt(i));
+    for(i=0;i<PLAINTEXTLEN;i++) {
+	if(encryptionTechnique.charAt(i) === "0") {
+	    cipherText += XOR(plaintext.charAt(i), key.charAt(i));
 	}
 	else {
-	    cipher_text += AND(plaintext.charAt(i), key.charAt(i));
+	    cipherText += AND(plaintext.charAt(i), key.charAt(i));
 	}
     }
-    return cipher_text;
+    return cipherText;
 }
 
-function Next_Encryption() {
-    current_encryption = Vernam_RandSequence(PLAIN_TEXT_LEN);
+function NextEncryption() {
+    currentEncryption = VernamRandSequence(PLAINTEXTLEN);
 }
 
 
-function next_plain_text() {
-    document.getElementById("plainarea").value = Vernam_RandSequence(PLAIN_TEXT_LEN);
+function nextPlainText() {
+    document.getElementById("plainarea").value = VernamRandSequence(PLAINTEXTLEN);
 }
 
-function next_key() {
-    key_len = (Math.random()*100000)%7 + 6;
-    document.getElementById("keyarea_gen").value = Vernam_RandSequence(key_len);
+function nextKey() {
+    keyLen = (Math.random()*100000)%7 + 6;
+    document.getElementById("keyareaGen").value = VernamRandSequence(keyLen);
 }
 
-function check_key() {
-    var key = document.getElementById("keyarea_gen").value;
+function checkKey() {
+    var key = document.getElementById("keyareaGen").value;
     var zero = 0;
     var one = 0;
     if (key === "") {
@@ -193,18 +193,18 @@ function check_key() {
 	    one ++;
 	} else {
 	    alert("Invalid Key. Please enter a binary key.");
-    	    document.getElementById("keyarea_gen").value = "";
+    	    document.getElementById("keyareaGen").value = "";
 	}
     }
     if (zero === one) {
-        document.getElementById("key_notify").value = "Correct Key! You may proceed.";
+        document.getElementById("keyNotify").value = "Correct Key! You may proceed.";
     } else {
-        document.getElementById("key_notify").value = "Wrong Key!";
+        document.getElementById("keyNotify").value = "Wrong Key!";
     }
 }
  
-function Encrypt_p() {
-    var k = document.getElementById("keyarea_gen").value;
+function EncryptP() {
+    var k = document.getElementById("keyareaGen").value;
     if (k === "") {
 	alert("Please calculate the new key with p=0.5");
 	return;
@@ -212,64 +212,64 @@ function Encrypt_p() {
     document.getElementById("cipherarea").value = encrypt(document.getElementById("plainarea").value, k);
 }
 
-function Next_Vernam_Test() {
-    document.getElementById("plainarea").value = Vernam_RandSequence(PLAIN_TEXT_LEN);
-    key_len = (Math.random()*100000)%7 + 6;
-    document.getElementById("keyarea_gen").value = Vernam_RandSequence(key_len);
-    Next_Encryption();
+function NextVernamTest() {
+    document.getElementById("plainarea").value = VernamRandSequence(PLAINTEXTLEN);
+    keyLen = (Math.random()*100000)%7 + 6;
+    document.getElementById("keyareaGen").value = VernamRandSequence(keyLen);
+    NextEncryption();
 }
 
-function next_binary_num(binary_num) {
+function nextBinaryNum(binaryNum) {
     var index = 0;
-    var len = binary_num.length;
+    var len = binaryNum.length;
     //replace the first "0" with "1"
-    if (index < len && binary_num.charAt(index) === "0") {
-	binary_num = "1" + binary_num.substring(1);
-     return binary_num;
+    if (index < len && binaryNum.charAt(index) === "0") {
+	binaryNum = "1" + binaryNum.substring(1);
+     return binaryNum;
     }
     //toggle all 1's
-    while (index < len && binary_num.charAt(index) === "1") {
-        var temp_binary_num = "";
+    while (index < len && binaryNum.charAt(index) === "1") {
+        var tempBinaryNum = "";
         if (index > 0) {
-            temp_binary_num += binary_num.substring(0,index);
+            tempBinaryNum += binaryNum.substring(0,index);
 	}
-	temp_binary_num += "0";
+	tempBinaryNum += "0";
 	if (index+1 < len) {
-	    temp_binary_num += binary_num.substring(index+1,len);
+	    tempBinaryNum += binaryNum.substring(index+1,len);
 	}
-        binary_num = temp_binary_num;
+        binaryNum = tempBinaryNum;
 	index ++;
     }
     //toggle next 0
     if (index < len) {
-	var temp_binary_num = "";
-        temp_binary_num += binary_num.substring(0,index) + "1";
+	var tempBinaryNum = "";
+        tempBinaryNum += binaryNum.substring(0,index) + "1";
 	if (index+1 < len) {
-	    temp_binary_num += binary_num.substring(index+1);
+	    tempBinaryNum += binaryNum.substring(index+1);
 	}
-        binary_num = temp_binary_num;
+        binaryNum = tempBinaryNum;
     }
-    return binary_num;
+    return binaryNum;
 }
 
-function generate_all_pairs() {
-    var key = document.getElementById("user_key").value;
+function generateAllPairs() {
+    var key = document.getElementById("userKey").value;
     if (key.length === 0) {
 	alert("Please enter a binary key!");
         return;
     }
-    key = resize_key(key);
-    var all_tuples = "";
-    var binary_num = "00000000";
-    var possible_plain_text= Math.pow(2,PLAIN_TEXT_LEN);
-    possible_plain_text= Math.pow(2,PLAIN_TEXT_LEN);
+    key = resizeKey(key);
+    var allTuples = "";
+    var binaryNum = "00000000";
+    var possiblePlainText= Math.pow(2,PLAINTEXTLEN);
+    possiblePlainText= Math.pow(2,PLAINTEXTLEN);
     var i = 0;
-    for (i=0;i<possible_plain_text;i++) {
-	binary_num = next_binary_num(binary_num);
-	crypted_text = encrypt(binary_num, key);
-        all_tuples += binary_num + " , " + crypted_text + "\n" ;
+    for (i=0;i<possiblelainText;i++) {
+	binaryNum = nextBinaryNum(binaryNum);
+	cryptedText = encrypt(binaryNum, key);
+        allTuples += binaryNum + " , " + cryptedText + "\n" ;
     }
-    document.getElementById("textarea2").value = all_tuples;
+    document.getElementById("textarea2").value = allTuples;
 }
 
 function checkAnswer() {
@@ -284,8 +284,8 @@ function checkAnswer() {
 	alert("Please enter values for m1 and m2");
 	return;
     }
-    var key = document.getElementById("user_key").value;
-    key = resize_key(key);
+    var key = document.getElementById("userKey").value;
+    key = resizeKey(key);
     var c1 = encrypt(m1, key);
     var c2 = encrypt(m2, key);
     if(c1 === c2) {
