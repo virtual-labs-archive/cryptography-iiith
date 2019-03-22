b15ed4cebab783d31e0cc2d5ad427d4ae757460a
doltonfernandes <42113482+doltonfernandes@users.noreply.github.com>
2019-03-22 09:38:51 +0530

    Updated exp1.js

--- src/lab/js/exp1.js
+++ src/lab/js/exp1.js
@@ -53,6 +53,10 @@ function Next_Shift_Test() {
 } 
 
 function CheckAnswer() {
+	if(document.getElementById("textarea3").value=="")
+	{
+		alert("Please enter an input");
+	}
 	if (trim(document.getElementById("textarea3").value.toLowerCase()) == trim(answers[current_cipher]) && document.getElementById("select_ans_shift").selectedIndex == shift_indices[current_cipher]) {
 		document.getElementById("notification").value = "CORRECT!!";
 	} else {
